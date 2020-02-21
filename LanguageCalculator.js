08f870c90d381aba3e813bcf60ebb6feebd0fba9
Nabil Ridhwanshah <imnabilridhwan@gmail.com>
2020-02-21 08:40:56 +0800

    Final Update

--- LanguageCalculator.js
+++ LanguageCalculator.js
@@ -1,21 +1,18 @@
 let fetch = require('node-fetch');
-function LanguageCalculator(user) {
-    return new Promise((resolve, reject) => {
-        fetch(`https://api.github.com/users/${user}/repos?per_page=100`)
-            .then(res => res.json())
-            .then(data => {
-                let languages = {};
-                data.forEach(repo => {
-                    let {
-                        language
-                    } = repo;
-                    if (language == null) language = "None"
-                    if (languages[language] == undefined) languages[language] = 0
-                    languages[language] = languages[language] + 1
-                })
-                resolve(languages)
-            })
+
+async function LanguageCalculator(user) {
+    let languages = {};
+
+    let response = await fetch(`https://api.github.com/users/${user}/repos?per_page=100`)
+    let data = await response.json();
+
+    data.forEach(repo => {
+        let {language} = repo;
+        if (language == null) language = "None"
+        if (languages[language] == undefined) languages[language] = 0
+        languages[language] = languages[language] + 1
     })
+    return languages
 }
 
 module.exports = LanguageCalculator
